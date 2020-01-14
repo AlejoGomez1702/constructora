b9d1511d68e07f618f190e40c20426b959f90f33
AlejoGomez1702 <luis.gomezc@autonoma.edu.co>
2020-01-13 23:58:38 -0500

    Lista la pagina inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes,RouterModule } from '@angular/router'
+// import { PruebaComponent } from './components/prueba/prueba.component';
+import { HomeComponent } from './components/home/home.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-
+const routes:Routes = [
+  {
+    path:'prueba',
+    component:FooterComponent
+  },
+  {
+    path:'home',
+    component:InicioComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { HomeComponent } from './components/home/home.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    HomeComponent,
+    InicioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
